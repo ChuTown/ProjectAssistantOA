482d47accb9615814d7884db1c0478f0b4c48ab8
ChuTown <mchu341@gmail.com>
2025-08-09 13:18:28 -0400

    Basic header and structure

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+export default function Header() {
+    return (
+        <header>
+            <h1>Project Assistant</h1>
+            <div className="actions">
+                <button className="btn btn--secondary">Log in</button>
+                <button className="btn btn--primary">Sign up</button>
+            </div>
+        </header>
+    )
+}
